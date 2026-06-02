9d5bc781e9bd54aa2b05654bbc1b3f232bbf02e1
Kukkapalli Siri <kukkapallisiri07@gmail.com>
2026-06-02 14:35:15 +0530

    Update ImageEncryption.java

--- ImageEncryption.java
+++ ImageEncryption.java
@@ -10,7 +10,6 @@ public class ImageEncryption {
         Scanner sc = new Scanner(System.in);
 
         try {
-
             System.out.print("Enter image path: ");
             String path = sc.nextLine();
 
@@ -18,6 +17,11 @@ public class ImageEncryption {
             int key = sc.nextInt();
 
             File file = new File(path);
+
+            if (!file.exists()) {
+                System.out.println("Image file not found!");
+                return;
+            }
 
             BufferedImage image = ImageIO.read(file);
 
@@ -29,14 +33,14 @@ public class ImageEncryption {
             int width = image.getWidth();
             int height = image.getHeight();
 
-            // ENCRYPT IMAGE
-            BufferedImage encrypted = new BufferedImage(width, height, image.getType());
+            BufferedImage encrypted = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
 
             for (int y = 0; y < height; y++) {
                 for (int x = 0; x < width; x++) {
 
                     int pixel = image.getRGB(x, y);
 
+                    int a = (pixel >> 24) & 0xff;
                     int r = (pixel >> 16) & 0xff;
                     int g = (pixel >> 8) & 0xff;
                     int b = pixel & 0xff;
@@ -45,7 +49,8 @@ public class ImageEncryption {
                     g = (g + key) % 256;
                     b = (b + key) % 256;
 
-                    int newPixel = (r << 16) | (g << 8) | b;
+                    int newPixel = (a << 24) | (r << 16) | (g << 8) | b;
+
                     encrypted.setRGB(x, y, newPixel);
                 }
             }
@@ -53,14 +58,15 @@ public class ImageEncryption {
             ImageIO.write(encrypted, "png", new File("encrypted.png"));
             System.out.println("Encrypted image saved!");
 
-            // DECRYPT IMAGE
-            BufferedImage decrypted = new BufferedImage(width, height, image.getType());
+            
+            BufferedImage decrypted = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
 
             for (int y = 0; y < height; y++) {
                 for (int x = 0; x < width; x++) {
 
                     int pixel = encrypted.getRGB(x, y);
 
+                    int a = (pixel >> 24) & 0xff;
                     int r = (pixel >> 16) & 0xff;
                     int g = (pixel >> 8) & 0xff;
                     int b = pixel & 0xff;
@@ -69,14 +75,13 @@ public class ImageEncryption {
                     g = (g - key + 256) % 256;
                     b = (b - key + 256) % 256;
 
-                    int newPixel = (r << 16) | (g << 8) | b;
+                    int newPixel = (a << 24) | (r << 16) | (g << 8) | b;
+
                     decrypted.setRGB(x, y, newPixel);
                 }
             }
 
             ImageIO.write(decrypted, "png", new File("decrypted.png"));
-
-           
             System.out.println("Decrypted image saved!");
 
         } catch (Exception e) {
